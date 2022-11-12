ab9ce46fcb081baaebf55ff3d434f2604bd91be1
Alex-YuYan <alex.yan@nyu.edu>
2022-11-12 13:38:15 -0500

    About page update

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,5 +1,15 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import aboutUs from "../images/AboutUs.jpeg"
 
 export default function About() {
-  return <h1>关于</h1>;
+  return (
+    <Grid container>
+      <Grid item xs={1}></Grid>
+      <Grid item xs={10}>
+        <img src={aboutUs} width="100%"></img>
+      </Grid>
+      <Grid item xs={1}></Grid>
+    </Grid>
+  )
 }
