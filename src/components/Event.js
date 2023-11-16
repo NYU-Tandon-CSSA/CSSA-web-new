21e75dbac9409c8eb32b32981dadd18d5f5090f9
Helen-Yuan0814 <Helenyuan04@163.com>
2023-11-16 17:37:55 -0500

    new event card

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,59 @@
 import * as React from 'react';
-import CardActionArea from '@mui/material/CardActionArea';
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography, Box } from '@material-ui/core';
+//import CardActionArea from '@mui/material/CardActionArea';
+//import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography, Box, CardCover } from '@material-ui/core';
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import CardCover from '@mui/joy/CardCover';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import Link from '@mui/joy/Link';
 
-function Event({ eventTitle, eventSubTitle ,date, description, image, url }) {
+
+function Event({ eventTitle, image, url }) {
+
   return (
-    <Card sx={{ width: '20rem', height: '12.5rem' }}>
+      <Card component="li" sx={{ minWidth: 300, flexGrow: 1 ,
+        '&:hover': { boxShadow: 'xl', borderColor: 'warning.softActiveColor', borderWidth:3, borderRadius: 10} }}>
+       
+        <CardCover >
+        
+          <img
+            src={image}
+            srcSet={image}
+            loading="lazy"
+            alt=""
+          />
+        </CardCover>
+        <CardCover
+        sx={{
+          background:
+            'linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 100px)',
+        }}
+      />
+        <CardContent>
+          
+          <Typography
+            level="body-lg"
+            fontWeight="lg"
+            textColor="#fff"
+            mt={{ xs: 12, sm: 18 }}
+          >
+            <Link
+            overlay
+            underline="none"
+            href={url}
+            target="_blank"
+            sx={{color: '#fff'}}
+            > 
+                {eventTitle}
+
+            </Link> 
+          </Typography>
+        </CardContent>
+      </Card>
+    )
+/*
+    <Card>
       <CardMedia
         component="img"
         image={image}
@@ -27,6 +76,8 @@ function Event({ eventTitle, eventSubTitle ,date, description, image, url }) {
       </CardActions>
     </Card>
   );
+  */
 }
 
 export default Event;
+
