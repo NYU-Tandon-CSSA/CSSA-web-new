46f4744a58fb4bfeb2945dfa27ec00db25a9a3fa
biubiubiupiu21 <197yjy@gmail.com>
2024-02-03 22:24:50 -0500

    about us page

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,9 +3,12 @@ import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography, Bo
 import aboutUs from "../images/AboutUs.jpeg"
 import Container from '@mui/material/Container';
 
+// images
+import mario from '../images/members/Mario.jpg'
+import tim from '../images/members/Tim.jpg'
 
-import image1 from '../images/members/image1.jpg'
-import IntroOfMembers from '../components/IntroOfMembers';
+import IntroOfMembers from '../components/IntroOfMembers.js';
+import IntroOfEboard from '../components/IntroOfEboard.js';
 
 
 
@@ -48,42 +51,42 @@ export default function About() {
 
           {/* Row 2 */}
           <Grid item xs={12} md={3} justifyContent="center">
-            <IntroOfMembers 
-              name = "Wang Minghe - Mario" 
+            <IntroOfEboard 
+              name = "王铭赫 - Mario" 
               position = "主席"
               year = "2024"
               intro="Major 数学但也很喜欢心理学，
                 参与Steinhart Development Psychology暑期研究；
                 经常参加志愿者活动；
                 偶尔打打篮球。"
-              image={image1}/>
+              image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "Jing Yuwei - Christina" 
+            <IntroOfEboard 
+                name = "荆雨薇 - Christina" 
                 position = "副主席"
                 year = "2024"
                 intro="近期刻苦专研琴技，但是雨点不协调的音乐爱好者&贪吃鬼"
-                image={image1}/>
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "Shi Haowei - Max" 
+            <IntroOfEboard 
+                name = "施皓威 - Max" 
                 position = "秘书"
                 year = "2024"
                 intro="喜欢玩游戏和露营的CS自闭秃头宅男"
-                image={image1}/>
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "Shi Haowei - Mia" 
+            <IntroOfEboard 
+                name = "宋梦雅 - Mia" 
                 position = "财务"
                 year = "2024"
                 intro="estp 幸运小可爱一枚"
-                image={image1}/>
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12}>
@@ -91,81 +94,124 @@ export default function About() {
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "Junyu Yao - Tim" 
+            <IntroOfEboard 
+                name = "姚俊煜 - Tim" 
                 position = "技术与职业发展部部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="不吃蘑菇"
+                image={tim}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "Yuan Linfei - Helen" 
+            <IntroOfEboard 
+                name = "苑林霏 - Helen" 
                 position = "技术与职业发展部副部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="目标是改变世界！明天开开心心"
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "" 
+            <IntroOfEboard 
+                name = "赵中泽 - Gerald" 
                 position = "活动部部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="又菜又爱玩儿的，snowboarder, skier, air-softer, baller, boxer, wheel-thrower, mechanic."
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "" 
+            <IntroOfEboard 
+                name = "张露文 - Devon" 
                 position = "活动部副部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="东北小松菜奈"
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "" 
+            <IntroOfEboard 
+                name = "宋一荞 - Elaine" 
                 position = "外联部部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="脑洞比黑洞更深不可测的美食家"
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "" 
+            <IntroOfEboard
+                name = "林之 - Zoey" 
                 position = "外联部副部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="ENFJ；烘培、钩针初学者"
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "" 
+            <IntroOfEboard 
+                name = "赵朗玥 - Iris" 
                 position = "宣传部部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="招财如意的妈妈"
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12} md={3}>
-            <IntroOfMembers 
-                name = "" 
+            <IntroOfEboard 
+                name = "曾海琪 - Stephanie" 
                 position = "宣传部副部长"
                 year = "2024"
-                intro=""
-                image={image1}/>
+                intro="debug失败的Trader Joe's小粉丝修炼厨艺ing"
+                image={mario}/>
           </Grid>
 
           <Grid item xs={12}>
             <Typography class = 'Silkscreen' style={{fontSize:"200%", textTransform: 'uppercase'}}>Web development team</Typography>
           </Grid>
           
+          <Grid item xs={12} md={6}>
+            <IntroOfMembers
+              name = "姚俊煜 - Tim"
+              major = "Computer Engineering"
+              year = "2024"
+              image = {tim}/>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <IntroOfMembers
+                name = "苑林霏 - Helen"
+                major = "Computer Science"
+                year = "2026"/>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <IntroOfMembers
+                name = "苑林霏 - Helen"
+                major = "Computer Science"
+                year = "2026"/>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <IntroOfMembers
+                name = "苑林霏 - Helen"
+                major = "Computer Science"
+                year = "2026"/>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <IntroOfMembers
+                name = "苑林霏 - Helen"
+                major = "Computer Science"
+                year = "2026"/>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <IntroOfMembers
+                name = "苑林霏 - Helen"
+                major = "Computer Science"
+                year = "2026"/>
+          </Grid>
+
         </Grid>
 
       </div>
