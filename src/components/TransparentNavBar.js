d70dc2e062247a53885d7025f7e338dd60199325
biubiubiupiu21 <197yjy@gmail.com>
2024-02-07 23:07:09 -0500

    fixed button in navbar

--- src/components/TransparentNavBar.js
+++ src/components/TransparentNavBar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AppBar, Toolbar, makeStyles, Typography, Box, Drawer, List} from '@material-ui/core';
-import { Link } from '@mui/material';
+// import { Link } from '@mui/material';
+import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton'
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
