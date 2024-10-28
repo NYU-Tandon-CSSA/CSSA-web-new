560049022da9ec98aedda5ecffd4b789beb03e55
AndrewZ <90226362+Andrew-NoTag@users.noreply.github.com>
2024-10-28 18:55:49 -0400

    Change photos displayed on the banner
    更新banner照片

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,12 +6,11 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { useState } from 'react';
 
 // images
-import JoinPoster from "../images/JoinUs2024.png";
-import groupPhoto from '../images/GroupPhoto.jpg'
-import Tandon from '../images/Tandon.jpg'
-import EventPhoto from '../images/Event.jpg';
-import group2 from '../images/Group2.jpg';
-
+import groupPhoto from '../images/Group_Photo_Fall2024.JPG'
+import tandonPhoto from '../images/Tandon.jpg'
+import lastManStandingPoster from '../images/last_man_standing.png'
+import boardGameNightPoster from '../images/boardgame_night.png'
+import feichengwuraoPoster from '../images/feichengwurao.png'
 
 
 function Banner() {
@@ -32,12 +31,21 @@ function Banner() {
         animation="slide"
         // Additional Carousel properties here
       >
+        {/* Item Lists */}
+        {/* 
+        1. fall 2024 group photo
+        2. Tandon photo
+        3. last man standing poster
+        4. board game night poster
+        5. fei cheng wu rao poster
+        */}
 
-        {/* Iteam 1 */}
+
+        {/* Item 1: Group photo */}
         <div style={{ position: 'relative', height: '100%' }}>
           <img 
-            src={JoinPoster} 
-            alt={`Join Us Poster`} 
+            src={groupPhoto} 
+            alt={`Group Photo 2024 Fall`} 
             style={{ width: '100%', height: '90vh', pointerEvents: "none", objectFit: "cover" }} 
           />
           <div
@@ -52,31 +60,14 @@ function Banner() {
           ></div>
         </div>
 
-        <div style={{ position: 'relative', height: '100%' }}>
-          <img 
-            src={Tandon} 
-            alt={`6 Metro`} 
-            style={{ width: '100%', height: '90vh', pointerEvents: "none", objectFit: "cover" }} 
-          />
-          <div
-            style={{
-              position: 'absolute',
-              bottom: 0,
-              left: 0,
-              width: '100%',
-              height: '40vh',
-              background: 'linear-gradient(rgba(255, 255, 255, 0), rgba(238, 238, 238, 1))',
-            }}
-          ></div>
-        </div>
         {/* Add more items here if you have multiple images */}
         
-        {/* Iteam 2 */}
+        {/* Item 2: Tandon Photo */}
 
         <div style={{ position: 'relative', height: '100%' }}>
           <img 
-            src={group2} 
-            alt={`Group Photo 2`} 
+            src={tandonPhoto} 
+            alt={`6 MetroTech`} 
             style={{ width: '100%', height: '90vh', pointerEvents: "none", objectFit: "cover" }} 
           />
           <div
@@ -91,12 +82,33 @@ function Banner() {
           ></div>
         </div>
 
-        {/* Iteam 3 */}
+
+        {/* Item 3: last man standing poster */}
 
         <div style={{ position: 'relative', height: '100%' }}>
           <img 
-            src={groupPhoto} 
-            alt={`groupPhoto`} 
+            src={lastManStandingPoster} 
+            alt={`Last Man Standing Poster`} 
+            style={{ width: '100%', height: '90vh', pointerEvents: "none", objectFit: "cover" }} 
+          />
+          <div
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              width: '100%',
+              height: '40vh',
+              background: 'linear-gradient(rgba(255, 255, 255, 0), rgba(238, 238, 238, 1))',
+            }}
+          ></div>
+        </div>
+
+        {/* Item 4: Board Game Night Poster */}
+
+        <div style={{ position: 'relative', height: '100%' }}>
+          <img 
+            src={boardGameNightPoster} 
+            alt={`Board Game Night Poster`} 
             style={{ width: '100%', height: '90vh', pointerEvents: "none", objectFit: "cover" }} 
           />
           <div
@@ -111,12 +123,12 @@ function Banner() {
           ></div>
         </div>   
 
-        {/* Iteam 4 */}
+        {/* Item 5: Feichenegwurao Poster */}
 
         <div style={{ position: 'relative', height: '100%' }}>
           <img 
-            src={EventPhoto} 
-            alt={`Event Photo`} 
+            src={feichengwuraoPoster} 
+            alt={`Feichengwurao Poster`} 
             style={{ width: '100%', height: '90vh', pointerEvents: "none", objectFit: "cover" }} 
           />
           <div
