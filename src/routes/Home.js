15cde17dcfcee643009764a28b3b2c599d1bc021
winniewu550 <wnwu.winnie2016@gmail.com>
2024-11-12 15:27:41 -0500

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -94,6 +94,7 @@ function Home() {
           <Typography variant="h4" component="div" gutterBottom style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '2.3rem' }}>
             我们的活动
           </Typography>
+
         </Grid>
         {/* 非诚勿扰 */}
         <Grid item xs={12} md={6} lg={4}>
@@ -112,7 +113,7 @@ function Home() {
             rel="noopener noreferrer" 
             style={{ textDecoration: 'none' }}
           >
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 10 }}>
               <CardActionArea>
                 <CardMedia
                   component="img"
