1c62c3eca808786ac9fce30217ac013f2ff5978f
Hongdao Meng <108969479+Mycroft-s@users.noreply.github.com>
2024-11-11 22:10:06 -0500

    Update AboutContainer.js

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,184 +1,86 @@
-import React from "react";
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography, Box, Paper} from '@material-ui/core';
-import aboutUs from "../images/AboutUs.jpeg"
-import Container from '@mui/material/Container';
+import React from 'react';
+import { Typography, Divider, Grid, Box, Stack, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
-// css
-import '../css/about.css'
-
-import IntroOfMembers from '../components/IntroOfMembers.js';
-import IntroOfEboard from '../components/IntroOfEboard.js';
-import IntroOfDevTeam from "../components/IntroOfDevTeam.js";
-
-import {devTeam, advisorTeam, wallOfFame, boardTeam, cabinetTeam}  from "../data/data.js"
-
-export default function About() {
+function AboutContainer({ title, paragraph, imageUrl, titleColor, paragraphColor }) {
   return (
-    <Container maxWidth="lg" sx={{marginBottom: "8%"}}>
-
-      <ul class="background">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul> 
-
-      <div style={{paddingTop: "130px", textAlign:"center"}}>
-
-        <Grid container spacing={2} justifyContent="left" alignItems="center">
-          {/* Row 1 */}
-          {/* <Grid item xs={4} justifyContent="center">
-            AboutUs
-          </Grid>
-
-          <Grid item xs={8}>
-            Image
-          </Grid> */}
-
-          <Grid item xs={12}>
-            <Typography class = 'Silkscreen' style={{fontSize:"210%", textTransform: 'uppercase'}}>Executive board<br /> 2024 - 2025</Typography>
-            {/* <Typography class = 'Silkscreen' style={{fontSize:"170%", textTransform: 'uppercase'}}>2023 - 2024</Typography> */}
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography class = 'Silkscreen' style={{fontSize:"200%"}}>CABINET</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <hr style={{
-              border: 'none',
-              height: '1px',
-              backgroundColor: 'black',
-              margin: '0 0 10px 0'
-            }} />
-          </Grid>
-
-          {/* Row 2 */}
-          {cabinetTeam.map(({name,position,year,major,intro,image},index)=> (
-            <Grid item xs={12} md={3} key={index}>
-              <IntroOfEboard 
-                name={name}
-                position={position}
-                year={year}
-                major={major}
-                intro={intro}
-                image={image}
-              />
-            </Grid>
-          ))}
-
-          <Grid item xs={12}>
-            <Typography class = 'Silkscreen' style={{fontSize:"200%"}}>BOARD</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <hr style={{
-              border: 'none',
-              height: '1px',
-              backgroundColor: 'black',
-              margin: '0 0 10px 0'
-            }} />
-          </Grid>
-
-          {boardTeam.map(({name,position,year,major,intro,image},index)=> (
-            <Grid item xs={12} md={3} key={index}>
-              <IntroOfEboard 
-                name={name}
-                position={position}
-                year={year}
-                major={major}
-                intro={intro}
-                image={image}
-              />
-            </Grid>
-          ))}
-        
-          <Grid item xs={12}>
-            <Typography class = 'Silkscreen' style={{fontSize:"200%"}}>WALL OF FAME</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <hr style={{
-              border: 'none',
-              height: '1px',
-              backgroundColor: 'black',
-              margin: '0 0 10px 0'
-            }} />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography class='Silkscreen' style={{fontSize:"120%", marginBottom: '20px'}}>
-              欢迎点击卡片右下角链接connect Linkedin
-            </Typography>
-          </Grid>
-          {wallOfFame.map(({ name, position, year, major, intro, image, linkedinUrl }, index) => (
-            <Grid item xs={12} md={3} key={index}>
-              <IntroOfEboard 
-                name={name}
-                position={position}
-                year={year}
-                major={major}
-                intro={intro}
-                image={image}
-                linkedinUrl={linkedinUrl ? linkedinUrl : null}
-              />
-            </Grid>
-          ))}
-          
-          <Grid item xs={12}>
-            <Typography class = 'Silkscreen' style={{fontSize:"200%"}}>ADVISOR COMMITTEE</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <hr style={{
-              border: 'none',
-              height: '1px',
-              backgroundColor: 'black',
-              margin: '0 0 10px 0'
-            }} />
-          </Grid>
-
-          {advisorTeam.map(({name, position,year,major,intro,image},index)=>{
-            return (
-              <Grid item xs={12} md={3} key={index}>
-                <IntroOfEboard 
-                    name = {name}
-                    position = {position}
-                    year = {year}
-                    major = {major}
-                    intro = {intro}
-                    image={image}/>
-              </Grid>
-            )
-          })}
-
-          <Grid item xs={12}>
-            <Typography class = 'Silkscreen' style={{fontSize:"200%"}}>Web development team</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <hr style={{
-              border: 'none',
-              height: '1px',
-              backgroundColor: 'black',
-              margin: '0 0 10px 0'
-            }} />
-          </Grid>
-
-          {devTeam.map(({name, major, year, image},index) => {
-            return (
-              <Grid item xs={12} md={6} key={index}>
-                <IntroOfDevTeam
-                  name = {name}
-                  major = {major}
-                  year = {year}
-                  image = {image}/>
-              </Grid>
-            )
-          })}
+    <Box sx={{ padding: '32px', color: 'white' }}>
+      <Grid container spacing={4} alignItems="center">
+        {/* 左侧图片部分 */}
+        <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box
+            sx={{
+              boxShadow: 3,
+              margin: '16px',
+              overflow: 'hidden',
+              borderRadius: '8px',
+            }}
+          >
+            <img src={imageUrl} alt={title} style={{ width: '100%', height: 'auto', display: 'block', borderRadius: '8px' }} />
+          </Box>
         </Grid>
 
-      </div>
-    </Container>
-  )
+        {/* 右侧文字部分 */}
+        <Grid item xs={12} md={6} sx={{ textAlign: 'center' }}>
+          <Stack spacing={2} alignItems="center">
+            {/* 标题 */}
+            <Typography
+              variant="h4"
+              component="div"
+              gutterBottom
+              sx={{
+                fontWeight: 'bold',
+                fontSize: '2.3rem',
+                color: titleColor,
+              }}
+            >
+              {title}
+            </Typography>
+
+            {/* 分割线 */}
+            <Divider sx={{ width: '50%', backgroundColor: 'rgba(255, 255, 255, 0.3)', marginBottom: '16px' }} />
+
+            {/* 段落文字 */}
+            <Typography
+              variant="body1"
+              component="div"
+              sx={{
+                fontSize: '1.5rem',
+                lineHeight: 1.6,
+                color: paragraphColor,
+                maxWidth: '80%',
+              }}
+            >
+              {paragraph}
+            </Typography>
+          </Stack>
+        </Grid>
+      </Grid>
+
+      {/* 按钮部分 */}
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, marginTop: '150px' }}> {/* marginTop for adjust margin*/}
+        <Button
+          component={Link}
+          to="/about"
+          variant="contained"
+          sx={{ backgroundColor: "#57068c", color: '#FFF', '&:hover': { backgroundColor: "#450568" } }}
+        >
+          Learn More <DoubleArrowIcon />
+        </Button>
+
+        <Button
+          href="https://forms.gle/J7QJbiVAWX6nu9Je7"
+          target="_blank"
+          variant="contained"
+          sx={{ backgroundColor: "#57068c", color: '#FFF', '&:hover': { backgroundColor: "#450568" } }}
+        >
+          Join Us <AssignmentIndIcon />
+        </Button>
+      </Box>
+    </Box>
+  );
 }
+
+export default AboutContainer;
