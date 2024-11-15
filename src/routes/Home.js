4201ef1e1de5660c9f97c6efd8bb58d291ef000a
winniewu550 <wnwu.winnie2016@gmail.com>
2024-11-15 03:34:48 -0500

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,12 +6,11 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { makeStyles } from '@material-ui/core/styles';
 import { useEffect, useState } from 'react';
 import Event from '../components/Event';
-import Banner from '../components/Banner';
 // import Header from '../components/Header';
 
 // About us import
 import AboutContainer from '../components/AboutContainer';
-import groupPhoto from '../images/GroupPhoto.jpg';
+import groupPhoto from '../images/Group_Photo_Fall2024.JPG'
 import group1 from '../images/Group1.jpg';
 
 // Section for images of recent posts
@@ -21,7 +20,7 @@ import If_you_are_the_one from '../images/If_you_are_the_one.png';
 import Voice_of_NYC from '../images/Voice_of_NYC.jpg';
 import coffee_chat from '../images/coffee_chat.jpg';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   icon: {
     fontSize: '4rem !important',
     animation: '$jumpAnimation 1s linear infinite',
@@ -42,9 +41,51 @@ const useStyles = makeStyles(() => ({
   iconHidden: {
     opacity: 0,
   },
+  aboutUsSection: {
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#1c1c2d',
+    color: 'white',
+    padding: theme.spacing(4),
+    gap: theme.spacing(4),
+    boxSizing: 'border-box',
+    overflowX: 'hidden',
+  },
+  imageContainer: {
+    flex: 1,
+    height: '50%',
+    maxWidth: '600px',
+    margin: 'auto',
+    borderRadius: '16px',
+    overflow: 'hidden',
+    boxShadow: '0 8px 32px rgba(0, 0, 0, 0.3)',
+  },
+  contentContainer: {
+    flex: 1,
+    padding: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    maxWidth: '600px',
+    margin: 'auto',
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+    fontSize: '2.5rem',
+    fontWeight: 'bold',
+  },
+  paragraph: {
+    marginBottom: theme.spacing(4),
+    fontSize: '1rem',
+    lineHeight: 1.6,
+  },
+  buttonContainer: {
+    display: 'flex',
+    gap: theme.spacing(2),
+  },
 }));
-
-// Dynamic Navbar
 
 function Home() {
   const classes = useStyles();
@@ -74,17 +115,13 @@ function Home() {
 
   return (
     <div style={{ backgroundColor: '#EEEEEE', width: '100%' }}>
-      <Grid container spacing={3} style={{ width: '100%', margin: 0 }}>
       {/* Banner Image */}
+      <Grid container spacing={3} style={{ width: '100%', margin: 0 }}>
         <Grid 
           item 
           xs={12} 
           style={{ padding: 0, margin: 0, height: '100vh', position: 'relative', zIndex: 10 ,filter: 'brightness(1.2)' }}
         >
-          {/* <Banner /> */}
-          {/* <video autoPlay loop muted playsInline style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover', zIndex: '-1' }}>
-            <source src="/main theme.mp4" type="video/mp4" />
-          </video> */}
           <img 
             src="CSSAFLAG.jpg" 
             alt="Background" 
@@ -96,7 +133,6 @@ function Home() {
               zIndex: '-1'
             }} 
           />
-              {/* 文字内容 */}
           <div
             style={{
               position: 'relative',
@@ -109,19 +145,18 @@ function Home() {
               color: 'white',
               textAlign: 'center',
               padding: '0 20px',
-              backgroundColor: 'rgba(0, 0, 0, 0.3)' // 半透明背景，增强文字可读性
+              backgroundColor: 'rgba(0, 0, 0, 0.3)' // 半透明背景
             }}
           >
             <h1 style={{ fontSize: '5rem', fontWeight: 'bold' }}>NYU TANDON CSSA</h1>
           </div>
-            {/* 滚动按钮 */}
           <p 
             className="scrolldown" 
             style={{
               position: 'absolute',
-              bottom: '20px', // 距离底部 20px，可根据需要调整
+              bottom: '20px', 
               left: '50%',
-              transform: 'translateX(-50%)', // 水平居中
+              transform: 'translateX(-50%)',
               zIndex: 1
             }}
           >
@@ -141,22 +176,52 @@ function Home() {
               borderRadius: '50%',
               boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
             }}>
-              {/* <i className="icon-down-circle" style={{ fontSize: '24px', color: 'black' }}></i> */}
               <FontAwesomeIcon icon={faChevronDown} style={{ fontSize: '24px', color: 'black' }} />
-              {/* ▼ */}
             </a>
           </p>
         </Grid>
       </Grid>
+
       {/* 关于我们 */}
-      <div id='aboutUs' style={{ maxWidth: '85%', margin: '0 auto' }}>
-        <AboutContainer
-          title="关于我们"
-          paragraph="NYU Tandon CSSA（全称Chinese Students and Scholars Association，即中国学生学者联合会）是NYU Tandon School of Engineering最大的华人学生团体，同时也是中国驻纽约领事馆注册的学生组织。
-          我们面向NYU全体中国学生，以广大Tandon中国留学生为中心，致力于通过此平台为大家提供NYU校内及校外的优质资源与服务。"
-          imageUrl={group1}
+      <Box className={classes.aboutUsSection} id="aboutUs">
+        <Box 
+          className={classes.imageContainer} 
+          component={Paper}
+          elevation={3}
+          style={{ 
+            backgroundImage: `url(${groupPhoto})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
         />
-      </div>
+        <Box className={classes.contentContainer}>
+          <Typography variant="h2" className={classes.title}>
+            关于我们
+          </Typography>
+          <Typography variant="body1" className={classes.paragraph}>
+            NYU Tandon CSSA（全称Chinese Students and Scholars Association，即中国学生学者联合会）是NYU Tandon School of Engineering最大的华人学生团体，同时也是中国驻纽约领事馆注册的学生组织。 我们面向NYU全体中国学生，以广大Tandon中国留学生为中心，致力于通过此平台为大家提供NYU校内及校外的优质资源与服务。
+          </Typography>
+          <Box className={classes.buttonContainer}>
+            <Button 
+              variant="contained" 
+              color="primary"
+              href="https://forms.gle/R4N9Jq2iuD72hr1K6"
+              target="_blank"//在新标签页打开
+              rel="noopener noreferrer"
+            >
+              加入我们
+            </Button>
+            <Button 
+              variant="outlined" 
+              color="primary"
+              href="https://nyutandoncssa.com/#/about"
+            >
+              了解我们
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+
 
       {/* Events */}
       <Grid container style={{ paddingTop: '8vh', paddingBottom: '8vh', maxWidth: '85%', margin: '0 auto' }} spacing={3}>
@@ -165,7 +230,6 @@ function Home() {
             我们的活动
           </Typography>
         </Grid>
-        {/* 非诚勿扰 */}
         <Grid item xs={12} md={6} lg={4}>
           <Event
             eventTitle="非诚勿扰"
@@ -173,8 +237,6 @@ function Home() {
             url='https://mp.weixin.qq.com/s/iKj8NE3CwyO9U-tZ-_eq6g'
           />
         </Grid>
-
-        {/* 心动的声音 */}
         <Grid item xs={12} md={6} lg={4}>
           <Event
             eventTitle="心动的声音"
@@ -182,8 +244,6 @@ function Home() {
             url='https://mp.weixin.qq.com/s/TfM_ZKyMJvkZcAJlt8gsaQ'
           />
         </Grid>
-
-        {/* 狼人杀大赛 */}
         <Grid item xs={12} md={6} lg={4}>
           <Event
             eventTitle="周五桌游夜"
@@ -191,8 +251,6 @@ function Home() {
             url='https://mp.weixin.qq.com/s/aGcqx2f3yRKFcND69erLpA'
           />
         </Grid>
-
-        {/* 校友会 */}
         <Grid item xs={12} md={6} lg={4}>
           <Event
             eventTitle="校友会"
@@ -200,8 +258,6 @@ function Home() {
             url='https://mp.weixin.qq.com/s/zUUUnodaWAIr6A8ygWayaA'
           />
         </Grid>
-
-        {/* Coffee Chat */}
         <Grid item xs={12} md={6} lg={4}>
           <Event
             eventTitle="Coffee Chat"
@@ -210,7 +266,6 @@ function Home() {
           />
         </Grid>
       </Grid>
-
     </div>
   );
 }
