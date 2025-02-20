87d884361a5d022e62dfd6e9df53fa71fb1bb706
AndrewZ <90226362+Andrew-NoTag@users.noreply.github.com>
2025-02-20 17:10:52 -0500

    Update Gallery.js

--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Grid, Container, Typography } from '@mui/material';
 
-// 动态导入 src/images/fhImage 目录中的所有图片
-// 注意：路径是相对于当前文件（src/routes/Gallery.js）的
-const importAll = (context) => context.keys().map(context);
+const importAll = (context) => context.keys().map((key) => context(key));
 const images = importAll(require.context('../images/Events', false, /\.(png|jpe?g|svg)$/));
 console.log('Loaded images:', images);
 
@@ -17,8 +15,7 @@ const Gallery = () => {
         {images.map((image, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
             <img
-              src={image.default} // 使用 image.default 获取图片 URL
-              alt={`Photo ${index + 1}`} // 简单的 alt 文本
+              src={image} 
               style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
             />
           </Grid>
