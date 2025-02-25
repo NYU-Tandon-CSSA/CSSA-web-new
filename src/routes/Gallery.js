3278c3b4beaf997b6c06cd33c3562690d533e21b
AndrewZ <90226362+Andrew-NoTag@users.noreply.github.com>
2025-02-25 18:35:54 -0500

    add gallery

--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Grid, Container, Typography } from '@mui/material';
+import React from "react";
+import { Grid, Container, Typography } from "@mui/material";
 
 const importAll = (context) => context.keys().map((key) => context(key));
-const images = importAll(require.context('../images/Events', false, /\.(png|jpe?g|svg)$/));
-console.log('Loaded images:', images);
+const images = importAll(
+  require.context("../images/gallery", false, /\.(png|jpe?g|svg)$/i)
+);
 
 const Gallery = () => {
   return (
-    <Container>
-      <Typography variant="h4" align="center" gutterBottom>
-        Photo Gallery
-      </Typography>
+    <Container style={{ marginTop: "100px", marginBottom: "50px" }}>
       <Grid container spacing={3}>
         {images.map((image, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
+          <Grid item xs={12} sm={6} md={4} key={index} style={{ display: 'flex', flexDirection: 'column' }}>
             <img
-              src={image} 
-              style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
+              src={image}
+              style={{ width: "100%", height: "300px", objectFit: "cover", borderRadius: "8px" }}
             />
           </Grid>
         ))}
